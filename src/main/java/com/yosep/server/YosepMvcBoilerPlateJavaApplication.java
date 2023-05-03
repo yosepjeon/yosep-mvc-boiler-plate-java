f628172b68bbd517ea2550a91a131258d6b06040
yosepjeon <enekelx1@naver.com>
2023-05-03 19:14:15 +0900

    Initial commit

---
+++ src/main/java/com/yosep/server/YosepMvcBoilerPlateJavaApplication.java
@@ -0,0 +1,13 @@
+package com.yosep.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YosepMvcBoilerPlateJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YosepMvcBoilerPlateJavaApplication.class, args);
+	}
+
+}
