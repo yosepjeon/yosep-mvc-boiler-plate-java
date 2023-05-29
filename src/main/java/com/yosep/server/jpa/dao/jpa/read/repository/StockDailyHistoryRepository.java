db72fc1f7009e0ce875e5a3503528986bcd00a3d
yosepjeon <enekelx1@naver.com>
2023-05-29 19:24:04 +0900

    git 계정 변경 테스트

--- src/main/java/com/yosep/server/jpa/dao/jpa/read/repository/StockDailyHistoryRepository.java
+++ src/main/java/com/yosep/server/jpa/dao/jpa/read/repository/StockDailyHistoryRepository.java
@@ -2,8 +2,9 @@ package com.yosep.server.jpa.dao.jpa.read.repository;
 
 import com.yosep.server.jpa.domain.StockDailyHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-//@Repository
+@Repository
 public interface StockDailyHistoryRepository extends JpaRepository<StockDailyHistoryEntity, Long> {
 
 }
